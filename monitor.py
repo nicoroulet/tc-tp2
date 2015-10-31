e089f03bbc79ce826627c9c15ff472f67adb4a9f
Shai <shai.bianchi@mercadolibre.com>
2015-10-31 17:58:26 -0300

    Versión súper-básica de monitor. Se ejecuta python monitor.py <ip>

--- monitor.py
+++ monitor.py
@@ -1,6 +1,7 @@
 import logging
-
 logging.getLogger('scapy.runtime').setLevel(logging.ERROR)
+
+from traceroute import trace
 
 from sys import argv
 from scapy.all import sr, IP, ICMP
@@ -8,39 +9,17 @@ from curses import *
 from time import sleep
 from threading import Thread
 
-TIMEOUT = 5
 SCR_START_POS = 2
 
 scr = initscr()
 scr.addstr(0, 0, "LE MONITOR."); scr.refresh()
 
-def traceroute(dst ,max = 30):
-	hops = []
-	for ttl in range(1, max):
-		ans, unans = sr(IP(dst = dst, ttl = ttl) / ICMP(), timeout = TIMEOUT, verbose = False)
-		if ans:
-			snt, rcv = ans[0]
-			hops.append({'ip' : rcv.src, 'rtt' : rcv.time - snt.sent_time})
-			if rcv.src == dst:
-				break
-		else:
-			hops.append(None)
-	return hops
-
-# def dots_msg(y, x, str = "ya va"):
-# 	scr.addstr(y, x, str)
-# 	dots_x_pos = x + len(str)
-# 	while True:
-# 		for i in range(0,3):
-# 			sleep(1)
-# 			scr.addstr(y, dots_x_pos + i, '.'); scr.refresh()
-
-# def monitor():
-
-# def interactive():
-
 while True:
-	output = traceroute(argv[1])
+	if len(argv) > 1:
+		output = trace(argv[1])
+	else:
+		output = trace()
+
 	for i, hop in enumerate(output):
 		ttl = i + 1
 		row = i + SCR_START_POS
--- traceroute.py
+++ traceroute.py
@@ -1,8 +1,13 @@
 from scapy.all import sr, IP, ICMP
 
-def traceroute(dst, max = 30):
+TIMEOUT = 5
+MAX_HOPS = 30
+
+example_ip = '181.15.96.29'
+
+def trace(dst = example_ip):
 	hops = []
-	for ttl in range(1, max):
+	for ttl in range(1, MAX_HOPS):
 		ans, unans = sr(IP(dst = dst, ttl = ttl) / ICMP(), timeout = TIMEOUT, verbose = False)
 		if ans:
 			snt, rcv = ans[0]
