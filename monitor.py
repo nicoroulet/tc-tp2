3a855180765945e9a73f9099dacc7c7f57eb1f67
Shai <shai.bianchi@mercadolibre.com>
2015-11-01 01:34:14 -0300

    Mensaje buena onda en pantalla

--- monitor.py
+++ monitor.py
@@ -9,24 +9,54 @@ from curses import *
 from time import sleep
 from threading import Thread
 
-SCR_START_POS = 2
+SCR_DATA_START_ROW = 2
+SCR_MSG_ROW = 1
+tracing_message = 'Tracing ' + argv[1]
 
 scr = initscr()
+curs_set(0)
 scr.addstr(0, 0, "LE MONITOR."); scr.refresh()
 
-while True:
-	if len(argv) > 1:
-		output = trace(argv[1])
-	else:
-		output = trace()
+def print_tracing_message(y, x):
+	scr.addstr(y, x, tracing_message)
+	dots_x_pos = x + len(tracing_message)
+	while True:
+		scr.addstr(y, dots_x_pos, ' '*3)
+		for i in range(0,3):
+			sleep(1)
+			scr.addstr(y, dots_x_pos + i, '.'); scr.refresh()
 
-	for i, hop in enumerate(output):
-		ttl = i + 1
-		row = i + SCR_START_POS
-		if not hop:
-			scr.addstr(row, 0, str(ttl) + '\t' + '*')
+def monitor():
+	total_monitor_its = 0
+	while True:
+		if len(argv) > 1:
+			output = trace(argv[1])
 		else:
-			scr.addstr(row, 0, str(ttl) + '\t' + str(hop['ip']) + '\t' + str(hop['rtt']))
-		scr.refresh()
-		
-endwin()
+			output = trace()
+		total_monitor_its += 1
+
+		total_monitor_its_msg = '[total iterations: ' + str(total_monitor_its) + ']'
+		scr.addstr(SCR_MSG_ROW, len(tracing_message + '...')+1, total_monitor_its_msg)
+
+		for i, hop in enumerate(output):
+			ttl = i + 1
+			row = i + SCR_DATA_START_ROW
+			if not hop:
+				scr.addstr(row, 0, str(ttl) + '\t' + '*')
+			else:
+				scr.addstr(row, 0, str(ttl) + '\t' + str(hop['ip']) + '\t' + str(hop['rtt']))
+			scr.refresh()
+
+monitor_thread = Thread(target = monitor)
+msg_thread = Thread(target = print_tracing_message, args = (SCR_MSG_ROW, 0))
+
+monitor_thread.start()
+msg_thread.start()
+
+raw(); cbreak()
+c = scr.getch()
+if c == 'q':
+	monitor_thread.stop()
+	msg_thread.stop()
+	nocbreak()
+	endwin()
