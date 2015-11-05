d0ff170e234154023863c15cebc885d0902a83e3
nico <nicoroulet@gmail.com>
2015-11-05 18:20:51 -0300

    clear screen para el monitor

--- monitor.py
+++ monitor.py
@@ -15,11 +15,15 @@ scr = initscr()
 scr.addstr(0, 0, "LE MONITOR."); scr.refresh()
 
 while True:
+	
 	if len(argv) > 1:
 		output = trace(argv[1])
 	else:
 		output = trace()
-
+	
+	scr.clear()
+	scr.addstr(0, 0, "LE MONITOR.")
+	
 	for i, hop in enumerate(output):
 		ttl = i + 1
 		row = i + SCR_START_POS
@@ -27,6 +31,6 @@ while True:
 			scr.addstr(row, 0, str(ttl) + '\t' + '*')
 		else:
 			scr.addstr(row, 0, str(ttl) + '\t' + str(hop['ip']) + '\t' + str(hop['rtt']))
-		scr.refresh()
-		
+	scr.refresh()
+	
 endwin()
