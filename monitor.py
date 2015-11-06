8d4910cafcec70479a5d97453cb0a856ec21cc1e
Francisco Giordano <frangio.1@gmail.com>
2015-11-06 04:25:29 -0300

    pone mas prolijo cuando corta con ctrl-c

--- monitor.py
+++ monitor.py
@@ -1,32 +1,32 @@
 from traceroute import trace
 
 from sys import argv
-from curses import *
-from time import sleep
-from threading import Thread
+from curses import initscr, endwin
 
 SCR_START_POS = 2
 
 scr = initscr()
 scr.addstr(0, 0, "LE MONITOR."); scr.refresh()
 
-while True:
-	
+try:
+    while True:
 	if len(argv) > 1:
-		output = trace(argv[1])
+	    output = trace(argv[1])
 	else:
-		output = trace()
-	
+	    output = trace()
+
 	scr.clear()
 	scr.addstr(0, 0, "LE MONITOR.")
-	
+
 	for i, hop in enumerate(output):
-		ttl = i + 1
-		row = i + SCR_START_POS
-		if not hop:
-			scr.addstr(row, 0, str(ttl) + '\t' + '*')
-		else:
-			scr.addstr(row, 0, str(ttl) + '\t' + str(hop['ip']) + '\t' + str(hop['rtt']))
+	    ttl = i + 1
+	    row = i + SCR_START_POS
+	    if not hop:
+		scr.addstr(row, 0, str(ttl) + '\t' + '*')
+	    else:
+		scr.addstr(row, 0, str(ttl) + '\t' + str(hop['ip']) + '\t' + str(hop['rtt']))
 	scr.refresh()
-	
-endwin()
+except KeyboardInterrupt:
+    pass
+finally:
+    endwin()
