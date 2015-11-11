ceeb8cc101027f69b745cf12b5414fb35279738a
Francisco Giordano <frangio.1@gmail.com>
2015-11-11 19:25:18 -0300

    agrega test de grubbs

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
+# coding=utf-8
+
 from common import load
 from stats import statistics
 from numpy import mean, std
 from scipy.stats import t
 from math import sqrt
 from scipy.stats.mstats import normaltest
+
+def grubbs(samples_rtt):
+	N = len(samples_rtt)
+	G = (max(samples_rtt) - mean(samples_rtt)) / std(samples_rtt)
+	a = 0.01
+	crit_val = t.isf(a / N, N - 2)
+	crit_reg = (crit_val ** 2 / (N - 2 + crit_val ** 2)) * (N - 1) / sqrt(N)
+	return G, a, crit_reg
 
 def test(log):
 	stats = statistics(log)
@@ -12,28 +22,37 @@ def test(log):
 	#	if stat:
 	#		print "%d \t %.8f \t %.8f \t %s \t %d " %(i+1, stat['rtt_m'], stat['rtt_std'], str(stat['d_rtt_m']) if 'd_rtt_m' in stat else "*" , stat['n'])
 
-	samples = [ stat['d_rtt_m'] for stat in stats if stat and 'd_rtt_m' in stat and stat['d_rtt_m'] != '*' and stat['d_rtt_m'] > 0 ]
+	samples = [ s for s in stats if s and 'd_rtt_m' in s and s['d_rtt_m'] != '*' and s['d_rtt_m'] > 0 ]
+
+	samples_rtt = [ s['d_rtt_m'] for s in samples ]
 
 	print "== Test de normalidad ==\n"
-	print "p-value = {}".format(normaltest(samples)[1])
+	print "p-value = {}\n".format(normaltest(samples_rtt)[1])
 
-	N = len(samples)
-	G = (max(samples) - mean(samples)) / std(samples)
-	a = 0.5
-	crit_val = t.isf(a / N, N - 2)
-	crit_reg = (crit_val ** 2 / (N - 2 + crit_val ** 2)) * (N - 1) / sqrt(N)
+	for k in range(len(samples)):
+		print "MAX: {}".format(max(samples_rtt))
+		G, a, G_crit = grubbs(samples_rtt)
 
-	print """
-== Test de outliers de Grubbs ==
+		hop_to = max((s for s in samples if s['d_rtt_m'] in samples_rtt),
+				key = lambda s: s['d_rtt_m'])['ip']
+		for i in range(len(stats) - 1):
+			if stats[i+1] and 'ip' in stats[i+1] and stats[i+1]['ip'] == hop_to:
+				hop_from = stats[i]['ip']
 
-H0: there are no outliers in the data
-Ha: the maximum value is an outlier
+		print """
+== Test de outliers de Grubbs #{} ==
 
-G = {}
-a = {}
+     G = {}
+     a = {}
+G_crit = {}
 
-Rechazar H0 si G > {}\
-""".format(G, a, crit_val, crit_reg)
+Hop: {} -> {}
+""".format(k, G, a, G_crit, hop_from, hop_to)
+
+		if G > G_crit:
+			samples_rtt.remove(max(samples_rtt))
+		else:
+			break
 
 if __name__ == "__main__":
 	log = load()
